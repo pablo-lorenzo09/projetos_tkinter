ca83cdf6fd4c2fa191bd75f87c1fb7780c63e72e
Pablo Moralez <pablo.moralez@aluno.senai.br>
2025-08-29 08:17:42 -0300

    aprendi a colocar comentario na função

--- 01_Hello_world copy.py
+++ 01_Hello_world copy.py
@@ -1,14 +1,12 @@
 import tkinter as tk
 
-
-
 janela = tk.Tk()
-janela.title("Hello world ")
-label2 = tk.Label(janela, text=f"")
+janela.title("SUPER BOM DIA")
 janela.geometry("800x600+100+50")
 janela.resizable(False, False)
 
 janela.configure(bg="#FACADA")
+
 
 label = tk.Label(janela, 
                  text="Qual o seu nome?",
@@ -16,12 +14,27 @@ label = tk.Label(janela,
                  font="papyrus",
                  foreground="blue")
 
+##
+
+label_b_dia = tk.Label (janela, 
+                        text = f"",
+                        font= ("Arial", 30),
+                        foreground = "pink")
+
+
+
 campo_texto = tk.Entry(janela)
 
-def acao_do_botao():
-    label2 = tk.Label(janela, text=f"Bom dia {campo_texto}")
+nome_digitado = campo_texto.get
+#função do botao
 
-botao = tk.Button (janela, text="clique em mim!", command= acao_do_botao)
+def desejar_bom_dia():
+    """Essa função pega o nome digitado na caixa de texto de deseja um bom dia"""
+
+#botao
+
+botao = tk.Button (janela, text="clique em mim!")
+
 
 
 
@@ -34,8 +47,7 @@ janela.iconbitmap("icon/adobe.ico")
 label.pack()
 botao.pack()
 campo_texto.pack()
-label2.pack()
-
+label_b_dia.pack(pady=20)
 janela.mainloop()
 
 
