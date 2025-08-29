0192514ed637daa67975e785eda84d53d68f70e3
Pablo Moralez <pablo.moralez@aluno.senai.br>
2025-08-29 09:43:11 -0300

    MODIFY: envelopei tudo dentro de classe e funções

--- 01_Hello_world_usando_tkbootstrap.py
+++ 01_Hello_world_usando_tkbootstrap.py
@@ -1,59 +1,65 @@
 import ttkbootstrap as tk
 
-janela = tk.Window(themename="vapor")
+class Janela_principal():
+    """Classe para a criação da janela principal"""
 
-janela.title("SUPER BOM DIA")
-janela.geometry("800x600+100+50")
-janela.resizable(False, False)
+    def __init__(self):
 
-janela.configure(bg="black")
+        self.janela = tk.Window(themename="vapor")
 
+        janela.title("SUPER BOM DIA")
+        janela.geometry("800x600+100+50")
+        janela.resizable(False, False)
 
-label = tk.Label(janela, 
-                 text="Qual o seu nome?",
-                 font=("Arial", 30),
-                 foreground="white"
-)
-
-##
-
-label_b_dia = tk.Label (janela, 
-                        text = f"",
-                        font= ("Arial", 30),
-                        foreground = "white")
+        janela.configure(bg="black")
 
 
+        label = tk.Label(janela, 
+                        text="Qual o seu nome?",
+                        font=("Arial", 30),
+                        foreground="white"
+        )
 
-campo_texto = tk.Entry(janela)
+        ##
 
-
-#função do botao
-
-def desejar_bom_dia():
-    """Essa função pega o nome digitado na caixa de texto de deseja um bom dia"""
-    nome = campo_texto.get()
-    label_b_dia.configure(text=f"Bom dia, {nome}!")
-#botao
-
-botao = tk.Button (janela, 
-                   text="clique em mim!",
-                   command= desejar_bom_dia,
-                   style="danger")
+        label_b_dia = tk.Label (janela, 
+                                text = f"",
+                                font= ("Arial", 30),
+                                foreground = "white")
 
 
 
+        campo_texto = tk.Entry(janela)
 
 
 
-#COLOCANDO ICONE
-janela.iconbitmap("icon/adobe.ico")
+        #botao
+
+        botao = tk.Button (janela, 
+                        text="clique em mim!",
+                        command= desejar_bom_dia,
+                        style="danger")
 
 
-label.pack()
-campo_texto.pack()
-botao.pack()
-label_b_dia.pack(pady=20)
 
-janela.mainloop()
+        #COLOCANDO ICONE
+        janela.iconbitmap("icon/adobe.ico")
 
 
+        label.pack()
+        campo_texto.pack()
+        botao.pack()
+        label_b_dia.pack(pady=20)
+
+    def run():
+        """Executa a janela"""
+        #Loop da janela
+        janela.mainloop()
+
+
+    #função do botao
+    def desejar_bom_dia():
+        """Essa função pega o nome digitado na caixa de texto de deseja um bom dia"""
+        
+        nome = campo_texto.get()
+        label_b_dia.configure(text=f"Bom dia, {nome}!")
